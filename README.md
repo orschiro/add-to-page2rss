a02efc8fc10d6a938fc519b1704aa963ac801998
Robert Orzanna <orschiro@gmail.com>
2013-08-08 22:06:04 +0200

    1.0: Initial release

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 add-to-page2rss
 ===============
 
-Chrome extension which creates a RSS feed for any page using page2rss.com
+Chrome extension which creates a RSS feed for any page using page2rss.com. This extensions is in no way affiliated to [page2rss.com](http://page2rss.com) but a separate extension.
+
+I created this extension due to a missing feature in the official [page2rss](https://chrome.google.com/webstore/detail/page2rss/lhikkakcmddjomilpjgdepfgmakpekeh?utm_source=chrome-ntp-icon) extension. Instead of a browser action it triggers a page action which can only be used whenever a page does not have any valid RSS feed. However, there are pages which might have a valid RSS feed but which is not the one you are looking for. In contast, my extension is triggered on any page using a browser action.
+
+# [Download](https://chrome.google.com/webstore/detail/defpibeopikfdlbmmoefcmepfbomgbab)
+
+# Changelog
+
+**1.0**
+
+- Initial release
--- background.js
+++ background.js
@@ -1,22 +1,34 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    //var create_feed_url = "http://page2rss.com/api/page?url=" + encodeURIComponent(tab.url); //+ "&callback=page2rss_bookmark_urlr" 
-    //http://page2rss.com/page?url=" + tab.url + "/";
+    // Original bookmark JS code
+    //(function(){if(!window.page2rss_bookmark_urlr)window.page2rss_bookmark_urlr=function(ur){if(ur.error)alert(ur.error);if(ur.page&&ur.page.page)location.href=ur.page.page};var r=document.getElementById('urlFormRequest');if(r)r.parentNode.removeChild(r);r=document.createElement('script');r.id='urlFormRequest';r.type='text/javascript';r.src='http://page2rss.com/api/page?url='+encodeURIComponent(location.href)+'&callback=page2rss_bookmark_urlr';document.body.appendChild(r);})();
+    
+    var create_feed_url = "http://page2rss.com/api/page?url=" + encodeURIComponent(tab.url); //+ "&callback=page2rss_bookmark_urlr" 
     var feed_url = "http://page2rss.com/page?url=" + tab.url;
-    // chrome.tabs.create({"url": create_feed_url}); 
-    // chrome.tabs.getSelected(null, function(tab) {
-    //         chrome.tabs.remove(tab.id);
-    //     });
-    var open_feed = function() {
-        chrome.tabs.create({"url": feed_url});
-    };
 
-    chrome.tabs.executeScript(null, {file: "create_feed_url.js"}, function(results) {
-        setTimeout(open_feed, 5000);
+    chrome.tabs.create({"url": create_feed_url, active: false}, function(tab) {
+        chrome.browserAction.setBadgeText({text: 'wait'});
+        setTimeout(function() { 
+            chrome.tabs.remove(tab.id, function(tab) {
+                chrome.browserAction.setBadgeText({text: ''});
+            }); 
+
+        }, 5000);
+
+        // chrome.tabs.onUpdated.addListener(function( tabId , info ) {
+        //     if ( info.status == "complete" ) {
+        //         // your code ...
+        //         chrome.tabs.remove(tab.id);
+        //     }
+        // });
     });
 
-    //setTimeout(open_feed, 5000);
-
-    open_feed();
-
-    //(function(){if(!window.page2rss_bookmark_urlr)window.page2rss_bookmark_urlr=function(ur){if(ur.error)alert(ur.error);if(ur.page&&ur.page.page)location.href=ur.page.page};var r=document.getElementById('urlFormRequest');if(r)r.parentNode.removeChild(r);r=document.createElement('script');r.id='urlFormRequest';r.type='text/javascript';r.src='http://page2rss.com/api/page?url='+encodeURIComponent(location.href)+'&callback=page2rss_bookmark_urlr';document.body.appendChild(r);})();
+    setTimeout(function() { 
+        chrome.tabs.create({"url": feed_url, active: true}, function(tab) {
+            chrome.tabs.onUpdated.addListener(function( tabId , info ) {
+                if ( info.status == "complete" ) {
+                    chrome.browserAction.setBadgeText({text: 'done', tabId: tabId});
+                }
+            });
+        }); }
+    , 1000);    
 });
